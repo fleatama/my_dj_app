892c01b5150494f47ce459a406700b1c8c23d634
fleata <fleatama@gmail.com>
2020-02-23 12:50:53 +0900

    change application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
-  def full_title(page_title = '')
-    base_title = "MyTube APP"
-    if page_title.empty?
-      base_title
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '') # メソッド定義とオプション引数
+    base_title = "MyTube APP" # 変数への代入
+    if page_title.empty? # 論理値テスト
+      base_title # 暗黙の戻り値
     else
-      page_tite + " | " + base_title
+      page_tite + " | " + base_title # 文字列の結合
     end
   end
 end
