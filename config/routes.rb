31ee32a195f2712cb69172ee7565a76b5ed89036
fleata <fleatama@gmail.com>
2020-02-28 23:01:24 +0900

    change _header.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'static_pages#home'
-  get  'static_pages/home'
+  root 'users#index'
+  # root 'static_pages#home'
+  # get  'static_pages/home'
   get  'help', to:'static_pages#help'
   get  'about', to:'static_pages#about'
   get  'contact', to:'static_pages#contact'
   get  '/', to: 'users#index'
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    post 'login', to: 'devise/sessions#create'
+    delete 'signout', to: 'devise/sessions#destroy'
+  end
 end
