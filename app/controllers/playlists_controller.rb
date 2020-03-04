caeb7257e976a1b72980c34e12fa75ddbc00a145
fleata <fleatama@gmail.com>
2020-03-04 17:14:46 +0900

    change playlists_controller.rb

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,5 +1,18 @@
 class PlaylistsController < ApplicationController
+  before_action :set_playlist, only: [:show, :edit, :update, :destroy]
+
   def index
+    @playlists = Playlist.all
+  end
+
+  def show
+  end
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def edit
   end
 
   def create
@@ -18,4 +31,36 @@ class PlaylistsController < ApplicationController
       end
     end
   end
+
+  def update
+    respond_to do |format|
+      if @playlist.update(playlist_params)
+        format.html { redirect_to @playlist, notice: 'Playlist was successfully updated.' }
+        format.json { render :show, status: :ok,
+        location: @playlist }
+      else
+        format.html { render :edt }
+        format.json { render json: @playlist.errors, status:
+        :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @playlist.destroy
+    respond_to do |format|
+      format.html { redirect_to playlists_url, notice: 'Playlist was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_playlist
+      @playlist = Playlist.find(params[:id])
+    end
+
+    def playlist_params
+      params.require(:playlist).permit(:content, :youtube_url)
+    end
 end
---
+++ app/views/playlists/_playlist.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! playlist, :id, :content, :youtube_url, :created_at, :updated_at
+json.url playlist_url(playlist, format: :json)
