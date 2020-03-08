78b3fc33c6f3513f6fc0487000eae7ec3944a633
fleata <fleatama@gmail.com>
2020-03-08 19:42:13 +0900

    changed user_profile_and_playlist

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
+    @playlist = current_user.playlists.build if user_signed_in?
   end
 
   def help
+  end
+
+  def about
+  end
+
+  def contact
   end
 end
