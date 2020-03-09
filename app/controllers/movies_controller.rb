24cf4432a2a0fc200281ee07b2fb763c7d16baab
fleata <fleatama@gmail.com>
2020-03-09 18:10:47 +0900

    create movies/index.html.erb show.html.erb edit.html.erb _movie.html.erb new.html.erb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,67 @@
 class MoviesController < ApplicationController
+  before_action :set_playlist, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!
+
+  def index
+    @playlists = Playlist.all.page(params[:page])
+  end
+
+  def show
+  end
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def edit
+  end
+
+  def create
+    @playlist = current_user.playlists.build(playlist_params)
+    url = params[:playlist][:youtube_url]
+    url = url.last(11)
+    @playlist.youtube_url = url
+
+    respond_to do |format|
+      if @playlist.save
+        format.html { redirect_to @playlist, notice: 'Playlst was successfully created.' }
+        format.json { render :show, status: :created, location: @playlist }
+      else
+        format.html { render :new }
+        format.json { render json: @playlist.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @playlist.update(playlist_params)
+        format.html { redirect_to @playlist, notice: 'Playlist was successfully updated.' }
+        format.json { render :show, status: :ok,
+        location: @playlist }
+      else
+        format.html { render :edt }
+        format.json { render json: @playlist.errors, status:
+        :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @playlist.destroy
+    respond_to do |format|
+      format.html { redirect_to playlists_url, notice: 'Playlist was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_playlist
+      @playlist = Playlist.find(params[:id])
+    end
+
+    def playlist_params
+      params.require(:playlist).permit(:content, :youtube_url)
+    end
 end
