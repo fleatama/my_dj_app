f875f450a3f5e1c1960a2a67c68a9c67f0d53f33
fleata <fleatama@gmail.com>
2020-03-09 15:04:11 +0900

    changed user_profile_and_playlist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.page(params[:page])
   end
 
   def show
     # @user = current_user
     @user = User.find(params[:id])
-    # @playlists = @user.playlists.page(params[:page])
+    @playlists = @user.playlists.page(params[:page])
   end
 end
