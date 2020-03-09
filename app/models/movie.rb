9fbc95afdbd984dcb37d69214cecf3876d7604a7
fleata <fleatama@gmail.com>
2020-03-09 19:41:12 +0900

    changed movies and playlists

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   belongs_to :user
+  belongs_to :playlists
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true
--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,6 @@
 class Playlist < ApplicationRecord
   belongs_to :user
+  belongs_to :movies
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true
