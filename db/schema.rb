e7a0ab2e163b9c38954b93c86d92a0000045eacb
fleatama <fleatama@gmail.com>
2020-05-15 16:55:30 +0900

    migrate remove playlist:text column from movies_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_090414) do
+ActiveRecord::Schema.define(version: 2020_05_15_075310) do
 
   create_table "movie_playlist_relations", force: :cascade do |t|
     t.integer "movie_id"
